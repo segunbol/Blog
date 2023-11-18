9a39bc9c69de04af1eae064860d785d67d38884b
emmanuel <segunbolawole@yahoo.com>
2023-11-18 16:14:48 +0100

    vite

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   server: {
     proxy: {
-      '/api': {
+      '/api/v1': {
         target: 'http://localhost:3000',
         secure: false,
       },

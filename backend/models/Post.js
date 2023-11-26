50d7c6da1ea3ec7a63b049fc5499a067004943cc
emmanuel <segunbolawole@yahoo.com>
2023-11-26 15:49:44 +0100

    Production address included

--- backend/models/Post.js
+++ backend/models/Post.js
@@ -21,8 +21,7 @@ const PostSchema = new mongoose.Schema(
       required: true,
     },
     userId: {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: "User",
+      type: String,
       required: true,
     },
     userImg: {
